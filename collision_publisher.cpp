996da9e55bffe09e95277b2404dcef14324c41ab
lcsr-owl <nnagururu@gmail.com>
2022-01-11 19:10:31 -0500

    working voxels removed publisher

---
+++ collision_publisher.cpp
@@ -0,0 +1,34 @@
+#include "collision_publisher.h"
+#include <ambf_server/RosComBase.h>
+
+using namespace std;
+
+DrillingPublisher::DrillingPublisher(string a_namespace, string a_plugin){
+    init(a_namespace, a_plugin);
+}
+
+DrillingPublisher::~DrillingPublisher(){
+    m_voxelsRemovedPub.shutdown();
+}
+
+void DrillingPublisher::init(string a_namespace, string a_plugin){
+    m_rosNode = afROSNode::getNode();
+    m_voxelsRemovedPub = m_rosNode-> advertise<vdrilling_msgs::points>(a_namespace + "/" + a_plugin + "/voxels_removed", 1);
+
+}
+
+void DrillingPublisher::voxelsRemoved(float ray[3], int vcolor[4], double time){
+    msg.sim_time = time;
+
+    std::vector<int> vec(vcolor, vcolor + 3);
+    msg.voxel_color = vec;
+
+    msg.voxel_removed.x = ray[0];
+    msg.voxel_removed.y = ray[1];
+    msg.voxel_removed.z = ray[2];
+
+
+    m_voxelsRemovedPub.publish(msg);
+
+}
+
---
+++ collision_publisher.h
@@ -0,0 +1,29 @@
+#ifndef COLLISION_PUBLISHER_H
+#define COLLISION_PUBLISHER_H
+
+#include "ros/ros.h"
+#include <string>
+#include <vdrilling_msgs/points.h>
+
+
+class DrillingPublisher{
+public:
+    DrillingPublisher(std::string a_namespace, std::string a_plugin);
+    ~DrillingPublisher();
+    void init(std::string a_namespace, std::string a_plugin);
+    ros::NodeHandle* m_rosNode;
+
+    void voxelsRemoved(float ray[3], int vcolor[4], double time);
+private:
+    ros::Publisher m_voxelsRemovedPub;
+    vdrilling_msgs::points msg;
+};
+
+#endif //VOLUMETRIC_PLUGIN_COLLISION_PUBLISHER_H
+
+/*
+std_msgs/Header header
+float64 sim_time
+geometry_msgs/Point voxel_removed
+int32[] voxel_color
+*/
