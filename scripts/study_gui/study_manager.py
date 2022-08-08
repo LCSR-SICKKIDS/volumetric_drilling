1b1e07870c3be3653f35c6b8eb5aeb1dc05d7357
Adnan Munawar <adnan.munawar@live.com>
2022-08-08 11:36:42 -0400

    Added experimental gui for user-study

---
+++ scripts/study_gui/study_manager.py
@@ -0,0 +1,56 @@
+import subprocess
+
+
+class StudyManager:
+    def __init__(self):
+        self.ambf_handle = None
+        self.pupil_service_handle = None
+
+    def start_simulation(self):
+        if not self.ambf_handle:
+            self._launch_simulator()
+        else:
+            poll = self.ambf_handle.poll()
+            if poll is None:
+                print('INFO! AMBF Simulator already running. Close it to reopen again')
+            else:
+                self._launch_simulator()
+
+    def _launch_simulator(self):
+        self.ambf_handle = None
+        self.ambf_handle = subprocess.Popen('ambf_simulator')
+
+    def close_simulation(self):
+        if self.ambf_handle:
+            self.ambf_handle.terminate()
+            self.ambf_handle = None
+
+    def start_pupil_service(self):
+        if not self.pupil_service_handle:
+            self._launch_pupil_service()
+        else:
+            poll = self.pupil_service_handle.poll()
+            if poll is None:
+                print('INFO! Pupil service already running. Close it to reopen again')
+            else:
+                self._launch_pupil_service()
+
+    def _launch_pupil_service(self):
+        self.pupil_service_handle = None
+        self.pupil_service_handle = subprocess.Popen('pupil_service')
+
+    def close_pupil_service(self):
+        if self.pupil_service_handle:
+            self.pupil_service_handle.terminate()
+            self.pupil_service_handle = None
+
+    def close(self):
+        self.close_simulation()
+        self.close_pupil_service()
+
+
+
+
+
+
+
