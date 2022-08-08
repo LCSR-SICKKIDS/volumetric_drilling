2b240f1a07fe097c60ec0f5ee32321ec5d45826e
Adnan Munawar <adnan.munawar@live.com>
2022-08-08 18:14:00 -0400

    Added .yaml file for gui and launch setup

---
+++ scripts/study_gui/gui_setup.py
@@ -0,0 +1,50 @@
+import yaml
+import pathlib
+
+
+class VolumeInfo:
+    def __init__(self, name, adf_path, icon_path):
+        self.name = name
+        self.adf_path = adf_path
+        self.icon_path = icon_path
+
+    def print_info(self):
+        print('ADF Path', self.adf_path)
+        print('Icon Path', self.icon_path)
+
+
+class SetupGUI:
+    def __init__(self, setup_filename):
+        self.setup_file_name = setup_filename
+        self.file_path = pathlib.Path(self.setup_file_name).parent.resolve()
+        self.yaml_file = open('gui_setup.yaml', 'r')
+
+        self.yaml_data = yaml.safe_load(self.yaml_file)
+        self.ambf_executable_path = self.yaml_data["ambf_executable_path"]
+        self.pupil_executable_path = self.yaml_data["pupil_executable_path"]
+        self.launch_file = pathlib.Path(self.yaml_data["launch_file_path"]).resolve()
+        self.volumes_info = []
+        volumes_data = self.yaml_data['volumes']
+        for v in volumes_data:
+            adf_path = pathlib.Path(self.file_path / self.yaml_data[v]['adf_path']).resolve()
+            icon_path = pathlib.Path(self.yaml_data[v]['icon_path']).resolve()
+            name = self.yaml_data[v]['name']
+            # print('Parsing: ', v, ' ADF Path: ', adf_path, ' Icon Path: ', icon_path)
+            new_volume_info = VolumeInfo(name, adf_path, icon_path)
+            self.volumes_info.append(new_volume_info)
+
+    def print_volumes_info(self):
+        for v in self.volumes_info:
+            print('-------')
+            v.print_info()
+
+
+def main():
+    gs = SetupGUI('gui_setup.yaml')
+    gs.print_volumes_info()
+
+
+if __name__ == "__main__()":
+    main()
+
+
---
+++ scripts/study_gui/study_gui.py
@@ -0,0 +1,76 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import sys
+from study_manager import StudyManager
+from gui_setup import SetupGUI
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('layout.ui', self)
+
+        self.gui_setup = SetupGUI('volumes_info.yaml')
+        self.study_manager = StudyManager(self.gui_setup.ambf_executable_path, self.gui_setup.pupil_executable_path)
+        self.active_volume_adf = ''
+
+        # Setup the grid layout for different volumes
+        self.volumes_grid = self.findChild(QtWidgets.QGridLayout, 'gridLayout')
+
+        for i in range(len(self.gui_setup.volumes_info)):
+            vinfo = self.gui_setup.volumes_info[i]
+            radio_button = QtWidgets.QRadioButton(vinfo.name)
+            # radio_button.setGeometry(200, 150, 100, 40)
+            radio_button.volume_name = vinfo.name
+            radio_button.volume_adf = str(vinfo.adf_path)
+            radio_button.toggled.connect(self.radio_button_volume_selection)
+            icon_path_str = str(vinfo.icon_path)
+            print(icon_path_str)
+            pixmap = QPixmap(icon_path_str)
+            label = QtWidgets.QLabel(self)
+            label.setText('Label')
+            label.setPixmap(pixmap)
+            self.volumes_grid.addWidget(radio_button, i, 0)
+            self.volumes_grid.addWidget(label, i, 1)
+            if i == 0:
+                radio_button.setChecked(True)
+
+        self.button_start_simulation = self.findChild(QtWidgets.QPushButton, 'button_start_simulation')
+        self.button_start_simulation.clicked.connect(self.pressed_start_simulation)
+
+        self.button_pupil_service = self.findChild(QtWidgets.QPushButton, 'button_pupil_service')
+        self.button_pupil_service.clicked.connect(self.pressed_pupil_service)
+
+        self.button_record_study = self.findChild(QtWidgets.QPushButton, 'button_record_study')
+        self.button_record_study.clicked.connect(self.pressed_record_study)
+
+        self.text_participant_name = self.findChild(QtWidgets.QTextEdit, 'textEdit_participant_name')
+        self.show()
+
+    def pressed_start_simulation(self):
+        args = ['--launch_file', str(self.gui_setup.launch_file), '-l', '0', '-a', self.active_volume_adf]
+        self.study_manager.start_simulation(args)
+
+    def pressed_pupil_service(self):
+        self.study_manager.start_pupil_service()
+
+    def pressed_record_study(self):
+        name = self.text_participant_name.toPlainText()
+        print('Will Start Recording Participant: ', name)
+
+    def radio_button_volume_selection(self):
+        button = self.sender()
+        if button.isChecked():
+            print('Active Volume is ', button.volume_name)
+            self.active_volume_adf = button.volume_adf
+
+    def closeEvent(self, event):
+        print('Terminate Called')
+        self.study_manager.close()
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
